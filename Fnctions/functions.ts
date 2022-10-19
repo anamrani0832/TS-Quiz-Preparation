d9f45964a389e6b3fb19b0c0befcd9390d2c3889
Anam Rani <Anam Rani>
2022-10-19 05:44:07 +0500

    the voide type of function

--- Fnctions/functions.ts
+++ Fnctions/functions.ts
@@ -1,8 +1,32 @@
-let seq = (num : number) => {
-    return num * num;
-}
-let greet = (person : string) => {
-    return `Hello ${person}!`;
-}
+let seq = (num: number) => {
+  return num * num;
+};
+let greet = (person: string) => {
+  return `Hello ${person}!`;
+};
+let doSomeThing = (name: string, age: number, isFunny: boolean) => {};
+doSomeThing("Anam", 24, false);
 seq(10);
 greet("Anam");
+
+// working With default ParaMeters
+let greeting = (name: string = "Anam") => {};
+greeting();
+greeting("Maham");
+
+// Return Type Annotation
+let add = (x: number, y: number): number => {
+  return x + y;
+};
+
+// Anonymous function contextual typing
+
+const colors = ['red', 'orange', 'green'];
+colors.map(color => {
+  return color.toUpperCase();
+});
+
+// the void type of function
+let printTwice = (msg: string): void => {
+  console.log(msg);
+}
