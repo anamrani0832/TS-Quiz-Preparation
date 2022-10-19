86fa7f35bbbb0da20616944e74e4f7766b79f062
Anam Rani <Anam Rani>
2022-10-19 20:56:52 +0500

    objects working

---
+++ objects/objeccts.ts
@@ -0,0 +1,30 @@
+// Working with object types
+
+// let printName = (person: { first: string; second: string }) => {
+//     console.log(`${person.first} and ${person.second}`);
+//   };
+  
+//   printName({ first: "anam", second: "Rani" });
+  
+  const randomCoord = (): { x: number; y: number } => {
+    return { x: Math.random(), y: Math.random() };
+  };
+// *********Excess Properties***********
+//   printName({ first: "anam", second: "Rani", age:45 });
+
+type Person = {
+    name: string,
+    age: number
+}
+
+let printName = (person:Person) => {
+    return `hey, ${person.name} and ${person.age}`
+}
+
+// Optional properties
+ type Dots = {
+    x: number,
+    y: number,
+    z?: string
+ }
+ const triangle: Dots = {x: 2, y:4}
