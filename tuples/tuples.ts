5c57464233b2f176d3281f0630c8325ac5fd424e
Anam Rani <Anam Rani>
2022-10-21 15:24:04 +0500

    tuples are added

---
+++ tuples/tuples.ts
@@ -0,0 +1,5 @@
+// Arrays
+let myArray: (number|string) [] = ['one',3,4,5,6, 'seven', 8];
+const color: number[] = [255, 255, 255];
+// Tuples
+const myTuples : [number, number, number] = [255, 255, 255];
