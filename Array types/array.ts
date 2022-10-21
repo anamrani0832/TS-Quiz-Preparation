8ce4e62676e19f2709476a9de6352bc684fa2b5f
Anam Rani <Anam Rani>
2022-10-21 14:37:52 +0500

    arrays are completed

--- Array types/array.ts
+++ Array types/array.ts
@@ -1,3 +1,9 @@
 let myFriends: string[] = ["anam", "google", "stackoverflow"];
 myFriends.push("docs");
 console.log(myFriends[2]);
+// 2D and 3D Array
+const My2dArray: string[][] = [
+  ["anam", "maham", "Hira"],
+  ["al", "piyaz", "gajar"],
+];
+
