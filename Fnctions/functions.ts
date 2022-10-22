94dde516be8c6f0f5cb66e7d583a769dc961bee0
Anam Rani <Anam Rani>
2022-10-22 13:16:32 +0500

    fucntions added

--- Fnctions/functions.ts
+++ Fnctions/functions.ts
@@ -1,43 +1,46 @@
-let seq = (num: number) => {
-  return num * num;
-};
-let greet = (person: string) => {
-  return `Hello ${person}!`;
-};
-let doSomeThing = (name: string, age: number, isFunny: boolean) => {};
-doSomeThing("Anam", 24, false);
-seq(10);
-greet("Anam");
+// let seq = (num: number) => {
+//   return num * num;
+// };
+// let greet = (person: string) => {
+//   return `Hello ${person}!`;
+// };
+// let doSomeThing = (name: string, age: number, isFunny: boolean) => {};
+// doSomeThing("Anam", 24, false);
+// seq(10);
+// greet("Anam");
 
-// working With default ParaMeters
-let greeting = (name: string = "Anam") => {};
-greeting();
-greeting("Maham");
+// // working With default ParaMeters
+// let greeting = (name: string = "Anam") => {};
+// greeting();
+// greeting("Maham");
 
-// Return Type Annotation
-let add = (x: number, y: number): number => {
-  return x + y;
-};
+// // Return Type Annotation
+// let add = (x: number, y: number): number => {
+//   return x + y;
+// };
 
-// Anonymous function contextual typing
+// // Anonymous function contextual typing
 
-const colors = ["red", "orange", "green"];
-colors.map((color) => {
-  return color.toUpperCase();
-});
+// const colors = ["red", "orange", "green"];
+// colors.map((color) => {
+//   return color.toUpperCase();
+// });
 
-// the void type of function
-let printTwice = (msg: string): void => {
-  console.log(msg);
-};
+// // the void type of function
+// let printTwice = (msg: string): void => {
+//   console.log(msg);
+// };
 
-// ******the never type of function *********
-// a function that doesn't finish running
-const neverStop = (): never => {
-  while (true) {
-    console.log("i'm still going");
-  }
-};
+// // ******the never type of function *********
+// // a function that doesn't finish running
+// const neverStop = (): never => {
+//   while (true) {
+//     console.log("i'm still going");
+//   }
+// };
 
 
 
+let typeChange = (str:string, num:number) : number =>{
+  return num * parseInt(str);
+}
